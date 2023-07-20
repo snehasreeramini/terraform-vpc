4daa334325cb2fabcaae1e6984402647cbf0165c
sneha <snehasreeramini@gmail.com>
2023-07-20 18:12:17 +0200

    creation of vpc

---
+++ env-dev/dev-backend.tfvars
@@ -0,0 +1,3 @@
+bucket =  "terraform-b6"
+key    =  "vpc/dev/terraform.tfstate"
+region =  "us-east-1"
--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 module "vpc" {
-  source="./vendor/modules/vpc"
+  source= "git::https://github.com/snehasreeramini/tf-module-vpc.git?ref=main"
+  VPC_CIDR = var.VPC_CIDR
 }
